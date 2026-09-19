d9b03345a3d61fa48fa2e9e0803938681bbe3565
RewidaHammad <ganterr14@gmail.com>
2026-09-20 01:38:59 +0300

    Delete ViewModel directory

--- ViewModel/ClienDashboard VM/DashboardViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScalaVerse.ViewModel.Client_Dashboard_VM
-{
-    public class DashboardViewModel
-    {
-        public StatisticsViewModel Statistics { get; set; }
-        public List<ProjectCardViewModel> RecentProjects { get; set; }
-        public string ClientName { get; set; }
-    }
-}
--- ViewModel/ClienDashboard VM/ProjectCardViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿public class ProjectCardViewModel
-{
-    public int ProjectId { get; set; }
-    public string ProjectName { get; set; }
-    public string Status { get; set; }
-    public int ProgressPercentage { get; set; }
-    // ... just basic info for preview
-}
